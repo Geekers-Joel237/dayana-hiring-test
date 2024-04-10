a3e25908b09a8eb3e1d1c1248be2525154b4095e
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2024-04-10 06:42:04 +0100

    [#person] feature: check already exist niu, add e2e test

--- src/main/java/com/example/dayanahiringtest/identity/person/infrastructure/spring/ControllerExceptionHandler.java
+++ src/main/java/com/example/dayanahiringtest/identity/person/infrastructure/spring/ControllerExceptionHandler.java
@@ -1,2 +1,17 @@
-package com.example.dayanahiringtest.identity.person.infrastructure.spring;public class ControllerExceptionHandler {
+package com.example.dayanahiringtest.identity.person.infrastructure.spring;
+
+import com.example.dayanahiringtest.identity.person.domain.exceptions.AlreadyExistNiuException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(
+            AlreadyExistNiuException.class
+    )
+    ResponseEntity<String> handleAlreadyExistNiuException(AlreadyExistNiuException e) {
+        return ResponseEntity.ok(e.getMessage());
+    }
 }
--- src/test/java/com/example/dayanahiringtest/identity/person/CreatePersonE2ETests.java
+++ src/test/java/com/example/dayanahiringtest/identity/person/CreatePersonE2ETests.java
@@ -2,19 +2,26 @@ package com.example.dayanahiringtest.identity.person;
 
 import com.example.dayanahiringtest.PostgreSqlTestConfiguration;
 import com.example.dayanahiringtest.identity.person.application.ports.PersonRepository;
+import com.example.dayanahiringtest.identity.person.domain.model.Person;
 import com.example.dayanahiringtest.identity.person.domain.viewmodel.IdResponse;
+import com.example.dayanahiringtest.identity.person.domain.vo.NameVo;
+import com.example.dayanahiringtest.identity.person.domain.vo.NiuVo;
 import com.example.dayanahiringtest.identity.person.infrastructure.spring.CreatePersonDto;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,6 +35,7 @@ public class CreatePersonE2ETests {
     private PersonRepository personRepository;
 
     @Test
+    @DirtiesContext
     public void shouldCreatePerson() throws Exception {
         var dto = new CreatePersonDto(
                 "P059116609705Y",
@@ -56,4 +64,33 @@ public class CreatePersonE2ETests {
         Assertions.assertEquals(dto.getBirthDate(), person.getBirthDate().toString());
 
     }
+
+    @Test
+    @DirtiesContext
+    public void shouldNotCreatePersonWhenNiuAlreadyExistThenThrowException() throws Exception {
+        var existingPerson = Person.create(
+                new NiuVo("P059116609705Y"),
+                new NameVo("John"),
+                new NameVo("Doe"),
+                LocalDate.now()
+        );
+        personRepository.save(existingPerson);
+
+        var dto = new CreatePersonDto(
+                existingPerson.getNiu().value(),
+                "John",
+                "Doe",
+                "1998-05-05"
+        );
+
+        mockMvc
+                .perform(MockMvcRequestBuilders.post("/persons")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+    }
+
+
 }
